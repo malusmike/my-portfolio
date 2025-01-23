206e24f77feb8c1b1ab7fea4922b5180b2668899
malusmike <32797603+malusmike@users.noreply.github.com>
2025-01-23 14:54:51 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (e) => {
+        alert("Vielen Dank für Ihre Nachricht! Ich melde mich so bald wie möglich.");
+    });
+});
